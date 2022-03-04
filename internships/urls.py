fb30c79e407445ecf629ead82f46330fb8dcc252
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-03-04 12:00:55 -0500

    connected 'home' and 'posts' urls and views

--- internships/urls.py
+++ internships/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('postings.urls'), name='home'),
+    path('posts', include('postings.urls'), name='posts'),
 ]
